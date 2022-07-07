055bfbd267bcd3f7629ff235240318e612d52442
Crowdbotics <team@crowdbotics.com>
2022-07-07 07:08:27 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/new_river_office_su_36071/MainActivity.java
@@ -0,0 +1,15 @@
+package com.new_river_office_su_36071;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "new_river_office_su_36071";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "new_river_office_su_36071",
+  url: "https://new_river_office_su_36071.botics.co",
+  api: "https://new_river_office_su_36071.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
